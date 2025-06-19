65b71efdecda84e8ad40aafb0588f21ab1674db3
Nom3o <b3lar00s@gmail.com>
2025-06-19 02:42:18 -0400

    practice.c

---
+++ first week/practice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* 
+
+✅ 1. Age-Based Access Checker
+
+Ask the user for their age and tell them if they can enter a club (must be 18+).
+
+Requirements:
+
+    Use int and if/else
+    Use printf and %d
+    
+    */
+
+int main(){
+
+int age = 19;
+if( age == 19){
+    printf("you are welcome to the club!!!");
+}
+else{
+    printf("You are not allowed in here!!");
+}
+
+
+
+    return 0;
+}
